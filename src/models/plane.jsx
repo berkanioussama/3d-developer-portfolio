ebcbc5ee9a4f917f23c62c1c912aed9e6164b150
berkanioussama <berkanioussama@gmail.com>
2024-03-07 01:14:36 +0100

    add island plane sky animation and rotation

---
+++ src/models/plane.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef} from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import planeScene from '../assets/3d/plane.glb'
+
+const Plane = ({ isRotating, ...props}) => {
+
+    const planeRef = useRef()
+    const { scene, animations } = useGLTF(planeScene)
+    const { actions } = useAnimations(animations, planeRef)
+
+    useEffect(() => {
+        if (isRotating) {
+            actions['Take 001'].play()
+        } else {
+            actions['Take 001'].stop()
+        }
+    }, [actions ,isRotating])
+
+    return (
+        <mesh ref={planeRef} {...props}>
+            <primitive object={scene} />
+        </mesh>
+    );
+}
+ 
+export default Plane;
--- src/scene/scene.jsx
+++ src/scene/scene.jsx
@@ -1,14 +1,18 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Loader from "../scene/loader";
 import Island from "../models/island";
 import Sky from "../models/sky";
+import Plane from "../models/plane";
 
 const Scene = () => {
 
+    const [isRotating, setIsRotating] = useState(false)
+    const [currentStage, setCurrentStage] = useState(1)
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null
-        let screnPosition = [0, -6.5, -43]
+        let screenPosition = [0, -6.5, -43]
         let rotation = [0.1, 4.7, 0]
 
         if (window.innerWidth < 768) {
@@ -17,25 +21,50 @@ const Scene = () => {
             screenScale = [1, 1, 1]
         }
 
-        return [screenScale, screnPosition, rotation]
+        return [screenScale, screenPosition, rotation]
     }
 
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
 
+    const adjustPlaneForScreenSize = () => {
+        let screenScale, screenPosition
+
+        if (window.innerWidth < 768) {
+            screenScale = [1.5, 1.5, 1.5]
+            screenPosition = [0, -1.5, 0]
+        } else {
+            screenScale = [3, 3, 3]
+            screenPosition = [0, -4, -4]
+        }
+
+        return [screenScale, screenPosition]
+    }
+
+    const [planeScale, planePosition] = adjustPlaneForScreenSize()
+
     return (
         <Canvas 
-            className="w-full h-svh bg-transparent"
+            className={`w-full h-svh bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
             camera={{ near: 0.1, far: 1000 }}
         >
             <directionalLight position={[1, 1, 1]} intensity={2}/>
             <ambientLight intensity={0.5} />
             <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
             <Suspense fallback={<Loader/>}>
-                <Sky/>
+                <Sky isRotating={isRotating} />
                 <Island
                     scale={islandScale}
                     position={islandPosition}
                     rotation={islandRotation}
+                    isRotating={isRotating}
+                    setIsRotating={setIsRotating}
+                    setCurrentStage={setCurrentStage}
+                />
+                <Plane
+                    scale={planeScale}
+                    position={planePosition}
+                    rotation={[0, 20, 0]}
+                    isRotating={isRotating}
                 />
             </Suspense>
         </Canvas>
